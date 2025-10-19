7927d2d73bd28e3ca123cdf0e843e1ba0956d7d2
MasanoriIwakura <iwakura.masa@gmail.com>
2025-10-19 17:53:31 +0900

    index.tsx: update route handler to log connection info and display request headers

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
-import { Hono } from 'hono'
-import { renderer } from './renderer'
+import { Hono } from "hono";
+import { renderer } from "./renderer";
+import { getConnInfo } from "hono/cloudflare-workers";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use(renderer)
+app.use(renderer);
 
-app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
-})
+app.get("/", (c) => {
+  const info = getConnInfo(c);
+  const headers = c.req.header();
 
-export default app
+  console.log({
+    url: c.req.url,
+    query: c.req.query(),
+    ip: info.remote.address || "unknown",
+  });
+
+  return c.render(
+    <ul>
+      {Object.entries(headers).map(([key, value]) => (
+        <li key={key}>
+          {key}: {value}
+        </li>
+      ))}
+    </ul>
+  );
+});
+
+export default app;
